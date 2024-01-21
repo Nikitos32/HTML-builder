4067aa2ee184cd1ef74e357ee113938d28037974
Nikita <nikita456nikita@yandex.ru>
2024-01-21 22:22:15 +0300

    fix: fix solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-fs.readFile('text.txt', function (err, data) {
-  if (err) {
-    return console.log(err);
+const stream = new fs.ReadStream('01-read-file/text.txt', {encoding: 'utf-8'});
+stream.on('readable', function () {
+  var data = stream.read();
+  if (data !== null) {
+    console.log(data);
   }
-  console.log(data.toString());
 });
